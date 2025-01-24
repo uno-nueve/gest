fe5b3e6bb0ed378a0f2676d43b68635714f52bc7
Luciano Montilla <unonueve95@gmail.com>
2025-01-24 17:57:37 -0300

    Update Card and Avatar components

--- src/components/common/card/card.tsx
+++ src/components/common/card/card.tsx
@@ -1,18 +1,15 @@
 import { TEstudiante } from "@/utils/mock-data";
 import { Link } from "react-router";
+import { Avatar } from "../profile/profile";
 
 export const Card = ({ estudiante }: { estudiante: TEstudiante }) => {
     const { _id, nombre, apellido, imagen } = estudiante;
     return (
         <Link
             to={`${_id}`}
-            className="p-4 rounded-2xl border border-[#ededed] bg-gradient-to-b from-neutral-50 to-white"
+            className="p-4 flex flex-col gap-2 rounded-2xl border border-[#ededed] bg-gradient-to-b from-neutral-50 to-white"
         >
-            <img
-                src={`data:image/jpeg;base64,${imagen?.data}`}
-                alt={`${nombre} ${apellido}`}
-                className="object-cover w-40 h-40 rounded-lg"
-            />
+            <Avatar nombre={nombre} apellido={apellido} imagen={imagen} variant="square" />
             <p className="line-clamp-1">
                 {nombre} {apellido}
             </p>
--- src/components/common/profile/profile.tsx
+++ src/components/common/profile/profile.tsx
@@ -1,16 +1,33 @@
-import { TImagen } from "@/utils/mock-data";
+import { Button } from "@/components/ui/button/button";
+import { TEstudiante, TImagen } from "@/utils/mock-data";
+import { Link } from "react-router";
+import { Field } from "../field/field";
+import { Table } from "../table/table";
+import { cva } from "class-variance-authority";
+import { cn } from "@/utils/cn";
 
-export const Profile = ({
-    imagen,
-    nombre,
-    apellido,
-}: {
+type TAvatar = {
     imagen: TImagen | undefined;
     nombre: string;
     apellido: string;
-}) => {
+    variant?: "square" | "round";
+};
+
+const avatarVariants = cva("flex items-center justify-center overflow-hidden bg-white ", {
+    variants: {
+        variant: {
+            round: "w-40 h-40 rounded-full",
+            square: "w-full h-full rounded-lg aspect-square",
+        },
+    },
+    defaultVariants: {
+        variant: "round",
+    },
+});
+
+export const Avatar = ({ imagen, nombre, apellido, variant }: TAvatar) => {
     return (
-        <div className="flex items-center justify-center w-40 h-40 overflow-hidden bg-white rounded-full">
+        <div className={cn(avatarVariants({ variant }))}>
             {imagen?.data ? (
                 <img
                     src={`data:image/jpeg;base64,${imagen?.data}`}
@@ -24,5 +41,39 @@ export const Profile = ({
                 </p>
             )}
         </div>
+    );
+};
+
+export const Profile = ({ data }: { data: TEstudiante }) => {
+    return (
+        <main>
+            <div className="flex gap-12 px-4 py-8 border-b border-neutral-300 max-h-[224px]">
+                <Avatar imagen={data.imagen} nombre={data.nombre} apellido={data.apellido} />
+                <div className="flex flex-col gap-4">
+                    <h1 className="text-[2.5rem] font-bold">
+                        {data.nombre} {data.apellido}
+                    </h1>
+                    <Link to="editar">
+                        <Button>Editar estudiante</Button>
+                    </Link>
+                </div>
+            </div>
+            <div className="flex min-h-[calc(100vh-384px)]">
+                <aside className="flex flex-col gap-4 px-4 py-8 border-r w-[280px] border-neutral-300">
+                    <h2 className="text-xl font-bold text-nowrap">Información personal</h2>
+                    <Field label="Email" value={data.email} />
+                    <Field label="Dirección" value={data.direccion} />
+                    <Field label="Tutor" value={data.tutor} />
+                    <Field label="Teléfono" value={data.telefono} />
+                </aside>
+                <div className="flex flex-col w-full gap-4 px-4 py-8">
+                    <h2 className="text-xl font-bold">Información académica</h2>
+                    <Field label="Año" value={`${data.año} "${data.division}"`} />
+                    <Field label="Docente" value={data.docente} />
+                    <Table label="Cursos" cursos={data.cursos} />
+                    <Field label="Observaciones" value={data.observaciones} variant="display" />
+                </div>
+            </div>
+        </main>
     );
 };
