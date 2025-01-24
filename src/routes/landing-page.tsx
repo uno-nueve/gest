914b816a1231973beb5b250510356bc25211d454
Luciano Montilla <unonueve95@gmail.com>
2025-01-24 19:53:14 -0300

    Update Landing page

--- src/routes/landing-page.tsx
+++ src/routes/landing-page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button/button";
+import { Link } from "react-router";
+
 export const LandingPage = () => {
-    return <div>LANDING</div>;
+    return (
+        <div className="h-screen text-neutral-800 bg-neutral-50 bg-image">
+            <div className="flex flex-col items-center h-screen justify-evenly">
+                <div className="flex flex-col items-center">
+                    <h1 className="font-rokkitt text-[10rem] leading-none font-bold">GEst.</h1>
+                    <h2 className="text-xl">Gestion estudiantil simplificada.</h2>
+                </div>
+                <Link to="login">
+                    <Button size="lg">Ingresar</Button>
+                </Link>
+            </div>
+        </div>
+    );
 };
