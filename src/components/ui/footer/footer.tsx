ab94f0843371bd7673902e7487ec00dc37ac8b09
Luciano Montilla <unonueve95@gmail.com>
2025-01-24 19:06:18 -0300

    Update footer

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,6 +1,14 @@
+import { useLocation } from "react-router";
+
 export const Footer = () => {
     const year = new Date().getFullYear();
-    return (
+    const location = useLocation();
+
+    return location.pathname !== "/" ? (
+        <footer className="border-t bg-neutral-50 border-neutral-300">
+            <div className="flex items-center justify-center h-[79px] max-w-screen-xl m-auto border-x border-neutral-300" />
+        </footer>
+    ) : (
         <footer className="text-white bg-neutral-800">
             <div className="flex items-center justify-center h-20 max-w-screen-xl m-auto border-x border-neutral-700">
                 <p>
