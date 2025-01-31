e2b3055900dc4cb5d9cb3631b9e73a7b9ca95f44
Luciano Montilla <unonueve95@gmail.com>
2025-01-31 17:35:11 -0300

    Update cursos page

---
+++ src/hooks/use-fetch-cursos.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useAxios } from "./use-axios";
+import { ESTUDIANTES } from "@/utils/urls";
+
+export type TCount = { label: string; amount: number };
+
+export const useFetchCursos = () => {
+    const [count, setCount] = useState<TCount[]>();
+    const { GET } = useAxios();
+    const cursos = [
+        "Matemática",
+        "Historia",
+        "Biología",
+        "Arte",
+        "Lengua",
+        "Educación Física",
+        "Física",
+        "Química",
+        "Inglés",
+    ];
+
+    useEffect(() => {
+        async function fetchCount() {
+            try {
+                const studentCount = await Promise.all(
+                    cursos.map(async (curso) => {
+                        const estudiantes = await GET(`${ESTUDIANTES}?curso=${curso}`);
+                        return { label: curso, amount: estudiantes.length };
+                    })
+                );
+                setCount(studentCount);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchCount();
+    }, []);
+
+    return { count };
+};
--- src/routes/cursos-page.tsx
+++ src/routes/cursos-page.tsx
@@ -1,38 +1,27 @@
-import { Button } from "@/components/ui/button/button";
-import { Dialog, Form, Input } from "@base-ui-components/react";
+import { TCount, useFetchCursos } from "@/hooks/use-fetch-cursos";
+
+const CursoCard = ({ label, amount }: TCount) => {
+    return (
+        <div className="p-8 rounded-2xl flex flex-col items-center gap-2 border border-[#ededed] bg-gradient-to-b from-neutral-50 to-white">
+            <div className="text-2xl font-bold ">{label}</div>
+            <div className=" text-8xl">{amount}</div>
+        </div>
+    );
+};
 
 export const CursosPage = () => {
+    const { count } = useFetchCursos();
+
     return (
         <div className="flex flex-col gap-4">
             <div className="sticky flex items-center justify-between w-full py-4 border-b top-20 border-neutral-300 bg-[rbga(250,250,250,0.4)] backdrop-blur">
                 <h1 className="text-2xl font-bold">Cursos</h1>
-                <Dialog.Root>
-                    <Dialog.Trigger render={<Button>Nuevo curso</Button>} />
-                    <Dialog.Portal>
-                        <Dialog.Backdrop className="fixed inset-0 transition-all duration-150 bg-black opacity-20" />
-                        <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-8 w-96 max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-4 transition-all duration-150">
-                            <Dialog.Title className="pb-4 text-lg font-bold border-b border-neutral-400">
-                                Añadir curso
-                            </Dialog.Title>
-                            <Form>
-                                <Input
-                                    required
-                                    placeholder="Educación Física"
-                                    className="w-full h-8 px-4 py-2 my-4 rounded-lg"
-                                    autoFocus
-                                />
-                                <div className="flex float-right gap-2">
-                                    <Dialog.Close
-                                        render={<Button variant="secondary">Cancelar</Button>}
-                                    />
-                                    <Button>Añadir</Button>
-                                </div>
-                            </Form>
-                        </Dialog.Popup>
-                    </Dialog.Portal>
-                </Dialog.Root>
             </div>
-            {/* add "Cursos" graphs here */}
+            <div className="grid grid-cols-[repeat(auto-fit,_minmax(320px,_1fr))] gap-4">
+                {count?.map(({ label, amount }) => (
+                    <CursoCard label={label} amount={amount} key={label} />
+                ))}
+            </div>
         </div>
     );
 };
