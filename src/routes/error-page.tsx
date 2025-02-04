9c81a6b89e42eab4067838899928c91ef6c8df2d
Luciano Montilla <unonueve95@gmail.com>
2025-02-04 17:07:37 -0300

    Update error page

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,3 +1,19 @@
+import { ArrowLeft } from "@/components/ui/svg/left-arrow";
+import { Link, useRouteError } from "react-router";
+
 export const ErrorPage = () => {
-    return <div>ERROR PAGE</div>;
+    const error: any = useRouteError();
+    console.error(error);
+
+    return (
+        <div className="flex items-center justify-center w-screen h-screen text-white bg-neutral-800">
+            <h1 className="text-2xl font-bold">Ocurrió un error inesperado</h1>
+            <p className="text-xl text-orange-500">
+                <i>{error.statusText || error.message}</i>
+            </p>
+            <Link to="/" className="flex items-center gap-2">
+                <ArrowLeft /> Volver al inicio
+            </Link>
+        </div>
+    );
 };
